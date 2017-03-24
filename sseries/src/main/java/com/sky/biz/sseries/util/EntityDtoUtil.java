9e22b27783899de61a59f50444c8997cc7c3cba4
iamsakon <sakon.bluechip@gmail.com>
2017-03-24 22:05:13 +0700

    remove problem source code
    remove problem source code
    -prodim

--- sseries/src/main/java/com/sky/biz/sseries/util/EntityDtoUtil.java
+++ sseries/src/main/java/com/sky/biz/sseries/util/EntityDtoUtil.java
@@ -1,21 +1,7 @@
 package com.sky.biz.sseries.util;
 
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
-
 import com.sky.biz.sseries.dto.AbstractDTO;
 import com.sky.biz.sseries.entity.AbstractEntity;
-import com.sky.biz.sseries.prodim.dto.AbstractProdimDTO;
-import com.sky.biz.sseries.prodim.dto.ProductCategoryDTO;
-import com.sky.biz.sseries.prodim.dto.ProductMasterDTO;
-import com.sky.biz.sseries.prodim.dto.ProductTypeDTO;
-import com.sky.biz.sseries.prodim.dto.UomDTO;
-import com.sky.biz.sseries.prodim.entity.AbstractProdimEntity;
-import com.sky.biz.sseries.prodim.entity.ProductCategoryEntity;
-import com.sky.biz.sseries.prodim.entity.ProductMasterEntity;
-import com.sky.biz.sseries.prodim.entity.ProductTypeEntity;
-import com.sky.biz.sseries.prodim.entity.UomEntity;
 
 public abstract class EntityDtoUtil {
 
@@ -25,11 +11,5 @@ public abstract class EntityDtoUtil {
 		}
 	}
 	
-	public static void getAbstractProdimDTO(AbstractProdimEntity entity,AbstractProdimDTO dto){
-		if(entity != null && dto != null){
-			//TODO Map from entity to dto
-			EntityDtoUtil.getAbstractDTO(entity, dto);
-		}
-	}
 	
 }
